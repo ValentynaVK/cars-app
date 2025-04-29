66602653979f06c3bbbdf2076decd6ba16fb345e
ValentynaVK <valia2526.vk@gmail.com>
2025-04-29 16:11:42 +0300

    style-fixed

--- src/components/Card/CarsItem/CarsItem.jsx
+++ src/components/Card/CarsItem/CarsItem.jsx
@@ -9,21 +9,32 @@ const CarsItem = ({ car }) => {
     dealer: { name, ratings },
   } = car;
 
+  const totalRating = ratings.reduce((acc, num) => acc + num);
+  const averageRating = totalRating / ratings.length;
+
   return (
     <div className={style.card}>
-      <div className={style.info}>
-        <h2>
-          {" "}
-          {brand} {model}, {year} рік
-        </h2>
-        <p>
-          {" "}
-          Дилер - {name}. Середній рейтинг - {ratings}{" "}
-        </p>
+      <div className={style.cardHeader}>
+        <div className={style.info}>
+          <h2>
+            {brand} {model}, {year} рік
+          </h2>
+          <p>
+            Дилер - {name}. Середній рейтинг - {averageRating.toFixed(2)}
+          </p>
+        </div>
+        <div className={style.priceBlock}>
+          <p className={style.price}> {price} USD </p>
+          <p className={style.available}> В наявності</p>
+        </div>
       </div>
-      <div className={style.priceBlock}>
-        <p className={style.price}> {price} USD </p>
-        <p className={style.available}> В наявності</p>
+      <div className={style.cardBody}>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt qui
+          repellendus hic minus perspiciatis odio iusto amet corporis
+          voluptates? Possimus sunt ea suscipit similique rem non nulla eaque
+          quo dolorem.
+        </p>
       </div>
     </div>
   );

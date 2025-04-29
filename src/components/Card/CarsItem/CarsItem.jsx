90c9044f09cec59e941b90548e3ff3bc026e5fb0
ValentynaVK <valia2526.vk@gmail.com>
2025-04-29 11:23:54 +0300

    card-done

--- src/components/Card/CarsItem/CarsItem.jsx
+++ src/components/Card/CarsItem/CarsItem.jsx
@@ -1,10 +1,30 @@
 import style from "./CarsItem.module.css";
 
 const CarsItem = ({ car }) => {
+  const {
+    brand,
+    model,
+    year,
+    price,
+    dealer: { name, ratings },
+  } = car;
+
   return (
-    <div>
-      {car.brand}
-      {car.model}
+    <div className={style.card}>
+      <div className={style.info}>
+        <h2>
+          {" "}
+          {brand} {model}, {year} рік
+        </h2>
+        <p>
+          {" "}
+          Дилер - {name}. Середній рейтинг - {ratings}{" "}
+        </p>
+      </div>
+      <div className={style.priceBlock}>
+        <p className={style.price}> {price} USD </p>
+        <p className={style.available}> В наявності</p>
+      </div>
     </div>
   );
 };

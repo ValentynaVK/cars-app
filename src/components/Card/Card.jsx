1f9c52da5bb01762b1417c4fb3034a89c1dc91ca
ValentynaVK <valia2526.vk@gmail.com>
2025-05-09 14:26:04 +0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,17 @@
 import style from "./Card.module.css";
 import cars from "../../data/cars";
 import CarsList from "./CarsList/CarsList";
+import { useState } from "react";
 
 const Card = () => {
+  const [sortedCars, setSortedCars] = useState(cars);
+  const sortCarsByAsc = ({ cars }) => {
+    setSortedCars(cars.toSorted((a, b) => a.price - b.price));
+  };
+
+  const sortCarsByDesc = ({ cars }) => {
+    setSortedCars(cars.toSorted((a, b) => b.price - a.price));
+  };
   return (
     <main className={style.main}>
       <h1 className={style.title}>Пошук автомобілів</h1>
@@ -10,7 +19,16 @@ const Card = () => {
         <p className={style.label}>В наявності</p>
         <p className={style.available}>{cars.length} автомобілів</p>
       </div>
-      <CarsList cars={cars} />
+
+      <button type="button" onClick={() => sortCarsByAsc({ cars })}>
+        Спочатку дешеві{" "}
+      </button>
+
+      <button type="button" onClick={() => sortCarsByDesc({ cars })}>
+        Спочатку дорогі
+      </button>
+
+      <CarsList cars={sortedCars} />
     </main>
   );
 };
